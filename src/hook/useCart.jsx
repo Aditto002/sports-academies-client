994e80223f8eeac97b2cc902490105bf8173e8f0
Aditto Koiri <adittokoiri002@gmail.com>
2023-06-12 10:47:03 +0600

    userauth

--- src/hook/useCart.jsx
+++ src/hook/useCart.jsx
@@ -1,13 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../providers/Authprovider';
+import useAxiosSecure from './useAxiosSecoure';
 
 const useCart = () =>{
     const {user} = useContext(AuthContext);
+    // const [axiosSecure] = useAxiosSecure();
+  const token = localStorage.getItem('access-token');
     const { refetch, data : cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () =>{
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+                headers:{
+                    authorization: `bearer ${token}`
+                }
+            })
             return response.json();
         }
       })
